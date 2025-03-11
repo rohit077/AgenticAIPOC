3b41c8ce32badf3cb521eced09212e144165f3c0
Rohit Roy Chowdhury <40126851+rohit077@users.noreply.github.com>
2025-03-11 18:29:12 +0530

    Update uiconfigfile.py

--- src/langgraph/UI/uiconfigfile.py
+++ src/langgraph/UI/uiconfigfile.py
@@ -17,10 +17,10 @@ class Config:
         return self.config["DEFAULT"].get("PAGE_TITLE")
 
     def get_llm_options(self):
-        return self.config["DEFAULT"].get("LLM_OPTIONS")#.split(", ")
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
     
     def get_usecase_options(self):
-        return self.config["DEFAULT"].get("USECASE_OPTIONS")#.split(", ")
+        return self.config["DEFAULT"].get("USE_CASE_OPTIONS").split(", ")
 
     def get_groq_model_options(self):
         return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
